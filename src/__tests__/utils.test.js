9bc0231f3790f38d20f9c6988503298c52473709
Edmund <edmundgachanja@gmail.com>
2025-01-22 21:59:12 +0300

    Completes the lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,29 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("return true for words with both uppercase letters and lowercase letters", () => {
+        const word = "RaCecar";
+        const result = isPalindrome(word);
+        expect(result).toBe(true);
+  });
+ 
+  it("returns false if the input is an empty string", () => {
+        const word = "";
+        const result = isPalindrome(word);
+        expect(result).toBe(false);
+  });
+
+  it("returns true when the word forward is same as itself backwards", () => {
+        const word = "racecar";
+        const result = isPalindrome(word);
+        expect(result).toBe(true);
+  });
+
+  it("returns false when the word is not the same forwards as it is backwards", () => {
+        const word = "car";
+        const result = isPalindrome(word);
+        expect(result).toBe(false);
+  });
+
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+const reverse = (string) => {
+    const reversed = string.split("").reverse().join("");
+    return reversed;
+}
+  
+ export function isPalindrome(word) {
+    const lowerCase = word.toLowerCase();
+    const reversed = reverse(lowerCase); 
+    return lowerCase === reversed && word !== ""
+  }
+
+  console.log(isPalindrome(""))
